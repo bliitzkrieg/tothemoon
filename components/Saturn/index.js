cddc7ec57c43e40b36d530d17e2718c994a92f06
bliitzkrieg <lucamatthewdecaprio@gmail.com>
2021-04-20 21:04:01 -0230

    more additions

--- components/Saturn/index.js
+++ components/Saturn/index.js
@@ -5,7 +5,7 @@ function Saturn() {
     <motion.img 
       initial={{ x: 1000, y: -800, scale: 1.3 }} 
       animate={{ x: 500, y: -320, scale: 0.6 }} 
-      transition={{ delay: 30, duration: 20 }}
+      transition={{ delay: 75, duration: 20 }}
       className="saturn" 
       src="/saturn.png" 
       alt="saturn" />
---
+++ components/Sides/index.js
@@ -0,0 +1,10 @@
+function Sides() {
+  return (
+    <React.Fragment>
+      <img className="left-side" src="./left.png" alt="left side"/>
+      <img className="right-side" src="./right.png" alt="right side" />
+    </React.Fragment>
+  );
+}
+
+export default Sides
