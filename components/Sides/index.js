f5d6e299409bfcd94c58792e105ff4ff50c17b17
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2021-04-20 19:45:06 -0400

    Update index.js

--- components/Sides/index.js
+++ components/Sides/index.js
@@ -1,8 +1,8 @@
 function Sides() {
   return (
     <React.Fragment>
-      <img className="left-side" src="./left.png" alt="left side"/>
-      <img className="right-side" src="./right.png" alt="right side" />
+      <img className="left-side" src="./LEFT.png" alt="left side"/>
+      <img className="right-side" src="./RIGHT.png" alt="right side" />
     </React.Fragment>
   );
 }
